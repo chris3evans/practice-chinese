5f0b12324bc5737da82f33ebed2bd9173ad81f6a
chris3evans <chris3evans.ce@gmail.com>
2022-09-10 10:07:52 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,55 @@
+///////////////////////// ENGLISH AND CHINESE DATA
+
+const pronouns = [
+  ["I", "ngor"],
+  ["you (singular)", "lee"],
+  ["he", "koi"],
+  ["she", "koi"],
+  ["we", "ngor dee"],
+  ["you (plural)", "lee dee"],
+  ["they", "koi dee"],
+];
+
+const objects = [
+  ["town", "guy"],
+  ["television", "din-see"],
+  ["clothes", "sam"],
+  ["phone", "din-wa"],
+  ["doctor", "yee-sang"],
+  ["car", "chair"],
+  ["shoes", "hair"],
+  ["socks", "mat"],
+  ["money", "chin"],
+  ["water", "soy"],
+  ["bed", "chong"],
+  ["house", "oh"],
+  ["home", "oh-kay"],
+  ["bowls", "wun"],
+  ["tea", "char"],
+  ["rice", "faan"],
+  ["breakfast", "joe-chaan"],
+  ["something", "yeah"],
+];
+
+const verbs = [
+  ["to want", "yiw"],
+  ["to want (informal)", "oi"],
+  ["to eat", "sic"],
+  ["to drink", "yum"],
+  ["to help", "bong"],
+  ["to go", "hoiii"],
+  ["to return", "faan"],
+  ["to cook", "jee"],
+  ["to dry", "ma"],
+  ["to watch", "tay"],
+  ["to see", "tay"],
+  ["to listen", "teng"],
+  ["to drive", "jar"],
+];
+
+const questions = [
+  ["who", "bin-gor"],
+  ["why", "dim-guy"],
+  ["when", "gee-see"],
+  ["where", "bin-dall"],
+];
